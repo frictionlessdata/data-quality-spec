098fcc4499a7efe2a334b966a12091e021336327
roll <eskarev@gmail.com>
2019-11-04 14:48:59 +0300

    Updated to eslint@4

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 const Promise = require('bluebird')
-, readFile = Promise.promisify(require('fs').readFile)
-, tv4 = require('tv4')
+const readFile = Promise.promisify(require('fs').readFile)
+const tv4 = require('tv4')
 
 Promise
   .all([readFile('spec.json'), readFile('scripts/schema.json')])
   .then((results) => {
     const spec = JSON.parse(results[0].toString('utf8'))
-    , schema = JSON.parse(results[1].toString('utf8'))
-    , valid = tv4.validate(spec, schema)
+    const schema = JSON.parse(results[1].toString('utf8'))
+    const valid = tv4.validate(spec, schema)
 
     if (!valid) {
       console.log(tv4.error)
